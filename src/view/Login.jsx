04093f7aed76ac65da08c1f3f85328be9712d84f
wells_xq <kang991005214@163.com>
2019-03-06 11:29:47 +0800

    s

--- src/view/Login.jsx
+++ src/view/Login.jsx
@@ -1,10 +1,10 @@
 /**
  * Created by hao.cheng on 2017/4/16.
  */
-import React from 'react';
+import React, {Component} from 'react';
 import './login.scss';
 
-class Login extends React.Component {
+class Login extends Component {
     /*componentWillMount () {
         const {receiveData = new Function} = this.props;
         receiveData(null, 'auth');
@@ -41,7 +41,7 @@ class Login extends React.Component {
     gitHub = () => {
         window.location.href = 'https://github.com/login/oauth/authorize?client_id=792cdcd244e98dcd2dee&redirect_uri=http://localhost:3006/&scope=user&state=reactAdmin';
     };*/
-    render () {
+    render() {
         // const { getFieldDecorator } = this.props.form;
         return (
             <div className="login">
@@ -49,38 +49,6 @@ class Login extends React.Component {
                     <div className="login-logo">
                         <span>React Admin</span>
                     </div>
-                    {/*<Form onSubmit={this.handleSubmit} style={{maxWidth: '300px'}}>
-                        <FormItem>
-                            {getFieldDecorator('userName', {
-                                rules: [{ required: true, message: '请输入用户名!' }],
-                            })(
-                                <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="管理员输入admin, 游客输入guest" />
-                            )}
-                        </FormItem>
-                        <FormItem>
-                            {getFieldDecorator('password', {
-                                rules: [{ required: true, message: '请输入密码!' }],
-                            })(
-                                <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="管理员输入admin, 游客输入guest" />
-                            )}
-                        </FormItem>
-                        <FormItem>
-                            {getFieldDecorator('remember', {
-                                valuePropName: 'checked',
-                                initialValue: true,
-                            })(
-                                <Checkbox>记住我</Checkbox>
-                            )}
-                            <a className="login-form-forgot" href="" style={{float: 'right'}}>忘记密码</a>
-                            <Button type="primary" htmlType="submit" className="login-form-button" style={{width: '100%'}}>
-                                登录
-                            </Button>
-                            <p style={{display: 'flex', justifyContent: 'space-between'}}>
-                                <a href="">或 现在就去注册!</a>
-                                <a onClick={this.gitHub} ><Icon type="github" />(第三方登录)</a>
-                            </p>
-                        </FormItem>
-                    </Form>*/}
                 </div>
             </div>
 
