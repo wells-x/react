7de0d707d126182c08d5bcd34ce04c7624ef47d7
wells <kang991005214@qq.com>
2019-05-21 19:04:54 +0800

    redux

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'redux'
+// import { Provider, connect } from 'react-redux'
+
+// Action
+const increaseAction = { type: 'increase' };
+
+// Reducer
+function counter(state = { count: 0 }, action) {
+  const count = state.count;
+  switch (action.type) {
+    case 'increase':
+      return { count: count + 1 };
+    default:
+      return state
+  }
+}
+
+// Store
+const store = createStore(counter);
+
+export default store;
+export {
+  store
+}
