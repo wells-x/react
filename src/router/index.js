e751eae4ca2cac5516c833a15259c19d26a59758
wells-x <kang991005214@163.com>
2019-07-01 10:34:54 +0800

    测试 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Login from '../view/login/Login'
 import Register from '../view/login/Register'
 import NotUse from '../view/not-use/NotUse'
 import Counter from '../view/counter'
-import {store} from "../store";
-import {Provider} from 'react-redux'
+import { store } from "../store";
+import { Provider } from 'react-redux'
 import Home from '../view/home/Home'
 import User from '../view/user/UserList'
 import Square from '../view/square'
@@ -14,14 +14,14 @@ export default () => (
   <Provider store={store}>
     <Router>
       <Switch>
-        <Route path="/login" component={Login}/>
-        <Route path="/user" component={User}/>
-        <Route path="/square" component={Square}/>
-        <Route path="/register" component={Register}/>
-        <Route path="/404" component={NotUse}/>
-        <Route path="/test" component={Counter}/>
-        <Route path="/" exact component={Home}/>
-        <Route path="*" render={() => <Redirect to="/404" replace/>}/>
+        <Route path="/login" component={Login} />
+        <Route path="/user" component={User} />
+        <Route path="/square" component={Square} />
+        <Route path="/register" component={Register} />
+        <Route path="/404" component={NotUse} />
+        <Route path="/test" component={Counter} />
+        <Route path="/" exact component={Home} />
+        <Route path="*" render={() => <Redirect to="/404" replace />} />
       </Switch>
     </Router>
   </Provider>
